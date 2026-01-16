ad8299ae4d60513d22a9c18fd1d702b9b0f5afbf
Vishwesh Shinde <vishweshshinde26@gmail.com>
2026-01-16 16:38:05 +0530

    Fix: Import Link component in ListPage

--- client/src/components/ListPage.jsx
+++ client/src/components/ListPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import VideoCard from './VideoCard';
 import SEO from './SEO';
